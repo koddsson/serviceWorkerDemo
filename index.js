dba985bfe5eead89658a4c449d57a97266705bde
Kristján Oddsson <koddsson@gmail.com>
2015-04-14 22:56:12 +0000

    Test just the notifications API.

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',
-      registration.scope);
-    Notification.requestPermission(function(permission) {
-      if (permission === 'granted') {
-        registration.showNotification('Service Worker', {
-          body: 'Hello from the Service Worker!',
-          icon: 'glasses36.png'
-        });
-      } else {
-        alert('whyyyyy?');
-      }
+Notification.requestPermission(function(permission) {
+  if (permission === 'granted') {
+    var notification = new Notification('Notifications API', {
+      body: 'Hello from the Notifications API!',
+      icon: 'glasses36.png'
     });
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
+    notification.onclick = function() {
+      notifiction.close();
+      window.open().close();
+      window.focus();
+    };
+  } else {
+    alert('whyyyyy?');
+  }
+});
